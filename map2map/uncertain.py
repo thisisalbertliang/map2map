e49a054b72daa67420affc5cf278be84204388ab
Albert Liang <albertliang00@gmail.com>
2022-10-26 23:09:41 -0400

    Change loss to MSE between displacements; Add violin & errorbar plots to MC Dropout

--- map2map/uncertain.py
+++ map2map/uncertain.py
@@ -2,6 +2,7 @@ import argparse
 from math import sqrt
 from pprint import pprint
 import sys
+from statistics import variance, mean
 import torch
 from torch.utils.data import DataLoader
 from torch import nn
@@ -10,7 +11,6 @@ from torch.utils.tensorboard import SummaryWriter
 
 from map2map import models
 from map2map.data.fields import FieldDataset
-from map2map.models import power
 from map2map.models.model import StyledVNet
 from map2map.models.narrow import narrow_cast
 from map2map.utils.imp import import_attr
@@ -72,15 +72,25 @@ def estimate_uncertainty(args: argparse.ArgumentParser):
     print('model state at epoch {} loaded from {}'.format(state['epoch'], args.load_state))
     del state
 
+    criterion = import_attr(
+        args.criterion,
+        torch.nn,
+        models,
+        callback_at=args.callback_at
+    )
+    criterion = criterion()
+    criterion.to(device)
+
     if args.square_cb:
-        square_cb(args, model, test_loader, device)
+        square_cb(args, model, criterion, test_loader, device)
     else:
-        monte_carlo_dropout(args, model, test_loader, device)
+        monte_carlo_dropout(args, model, criterion, test_loader, device)
 
 
 def monte_carlo_dropout(
     args: argparse.Namespace,
     model: StyledVNet,
+    criterion,
     loader: DataLoader,
     device: torch.device
 ):
@@ -88,61 +98,118 @@ def monte_carlo_dropout(
     model.enable_dropout(args.dropout_prob)
 
     logger = SummaryWriter()
+    log_step = 0
 
-    mse_loss = nn.MSELoss()
+    from itertools import islice
+
     with torch.no_grad():
-        field_ordinals = []
         losses = []
-        for i, data in enumerate(loader):
+        for i, data in enumerate(islice(loader, 10)):
             style, input, target = data['style'], data['input'], data['target']
 
             style = style.to(device, non_blocking=True)
             input = input.to(device, non_blocking=True)
             target = target.to(device, non_blocking=True)
 
+            samples = []
             for s in range(args.sample_size):
                 output = model(input, style)
-                _, lag_out, lag_tgt = narrow_cast(input, output, target)
-                _, P_lag_out, _ = power(lag_out)
-                _, P_lag_tgt, _ = power(lag_tgt)
+                _, output, target = narrow_cast(input, output, target)
 
-                field_ordinals.append(i)
-                loss = mse_loss(P_lag_out, P_lag_tgt).cpu().item()
-                losses.append(loss)
+                loss = criterion(output, target).cpu().item()
+                samples.append(loss)
 
                 logger.add_text(
                     tag=f"Monte Carlo Dropout (prob = {args.dropout_prob})",
-                    text_string=f"Field {i}: Sample {s}: Power Spectrum MSE Loss: {loss}",
-                    global_step=i * args.sample_size + s
+                    text_string=f"Field {i}: Sample {s}: Output Displacement MSE Loss: {loss}",
+                    global_step=log_step
                 )
-            
-    plt.scatter(
-        x=field_ordinals,
-        y=losses,
-        s=5
+                log_step += 1
+            losses.append(samples)
+    
+    _plot_violin(args, logger, log_step, losses=losses, num_fields_per_plot=10)
+    _plot_errorbarr(args, logger, log_step, losses=losses, num_fields_per_plot=10)
+
+    logger.flush()
+    logger.close()
+
+
+def _plot_errorbarr(args, logger, log_step, losses, num_fields_per_plot):
+    means = [mean(sample) for sample in losses]
+    variances = [variance(sample) for sample in losses]
+
+    for i in range(0, len(losses), num_fields_per_plot):
+        plt.errorbar(
+            x=[j for j in range(i, min(len(losses), i + num_fields_per_plot))],
+            y=means[i:i+num_fields_per_plot],
+            yerr=variances[i:i+num_fields_per_plot],
+            ls='none',
+            fmt='o'
+        )
+        plt.xlabel("Field (Ordinal)")
+        plt.ylabel("Output Displacement MSE Losses")
+        plt.title(f"Dropout Probability = {args.dropout_prob}:\nOutput Displacement MSE Losses v.s. Field ({i} ~ {i + num_fields_per_plot - 1})")
+        fig = plt.gcf()
+        logger.add_figure(f'fig/errorbar/dropout-prob-{args.dropout_prob}-displacement-mse-vs-field-{i}-{i + num_fields_per_plot - 1}', fig)
+
+    plt.errorbar(
+        x=[j for j in range(len(losses))],
+        y=means,
+        yerr=variances,
+        ls='none',
+        fmt='o'
     )
     plt.xlabel("Field (Ordinal)")
-    plt.ylabel("Power Spectrum MSE Losses")
-    plt.title(f"Dropout Probability = {args.dropout_prob}: Power Spectrum MSE Losses v.s. Field (Ordinal)")
+    plt.ylabel("Output Displacement MSE Losses")
+    plt.title(f"Dropout Probability = {args.dropout_prob}:\nOutput Displacement MSE Losses v.s. Field (All)")
     fig = plt.gcf()
+    logger.add_figure(f'fig/errorbar/dropout-prob-{args.dropout_prob}-displacement-mse-vs-field-all', fig)
 
-    logger.add_figure(f'fig/dropout-prob-{args.dropout_prob}-power-spec-mse-vs-field-ordinal', fig)
-    logger.flush()
+    logger.add_text(
+        tag=f"Monte Carlo Dropout (prob = {args.dropout_prob})",
+        text_string=f"Field with min MSE: {min((i for i in range(len(means))), key=lambda i: means[i])}",
+        global_step=log_step
+    )
+    log_step += 1
+    for i in range(len(losses)):
+        logger.add_text(
+            tag=f"Monte Carlo Dropout (prob = {args.dropout_prob})",
+            text_string=f"Field {i}: MSE Mean = {means[i]}; MSE Variance = {variances[i]}",
+            global_step=log_step
+        )
+        log_step += 1
+        logger.flush()
 
-    logger.close()
+
+def _plot_violin(args, logger, log_step, losses, num_fields_per_plot):
+    for i in range(0, len(losses), num_fields_per_plot):
+        plt.violinplot(losses[i : i + num_fields_per_plot])
+        plt.xlabel("Field (Ordinal)")
+        plt.ylabel("Output Displacement MSE Losses")
+        plt.title(f"Dropout Probability = {args.dropout_prob}:\nOutput Displacement MSE Losses v.s. Field ({i} ~ {i + num_fields_per_plot - 1})")
+        fig = plt.gcf()
+        logger.add_figure(f'fig/violin/dropout-prob-{args.dropout_prob}-displacement-mse-vs-field-{i}-{i + num_fields_per_plot - 1}', fig)
+
+    plt.violinplot(losses)
+    plt.xlabel("Field (Ordinal)")
+    plt.ylabel("Output Displacement MSE Losses")
+    plt.title(f"Dropout Probability = {args.dropout_prob}:\nOutput Displacement MSE Losses v.s. Field (All)")
+    fig = plt.gcf()
+    logger.add_figure(f'fig/violin/dropout-prob-{args.dropout_prob}-displacement-mse-vs-field-all', fig)
 
 
 def square_cb(
     args: argparse.Namespace,
     model: nn.Module,
+    criterion,
     loader: DataLoader,
     device: torch.device
 ):
     model.eval()
 
     logger = SummaryWriter()
+    log_step = 0
 
-    mse_loss = nn.MSELoss()
     with torch.no_grad():
         losses = []
         min_i, min_loss = None, float('inf')
@@ -154,11 +221,9 @@ def square_cb(
             target = target.to(device, non_blocking=True)
 
             output = model(input, style)
-            _, lag_out, lag_tgt = narrow_cast(input, output, target)
-            _, P_lag_out, _ = power(lag_out)
-            _, P_lag_tgt, _ = power(lag_tgt)
+            _, output, target = narrow_cast(input, output, target)
 
-            loss = mse_loss(P_lag_out, P_lag_tgt).cpu().item()
+            loss = criterion(output, target).cpu().item()
             if loss < min_loss:
                 min_i, min_loss = i, loss
 
@@ -166,12 +231,14 @@ def square_cb(
 
             logger.add_text(
                 tag="SquareCB",
-                text_string=f"Field {i}: Power Spectrum MSE Loss: {loss}",
-                global_step=i
+                text_string=f"Field {i}: Output Displacement MSE Loss: {loss}",
+                global_step=log_step
             )
+            log_step += 1
     
     mu = len(loader.dataset)
     gamma = sqrt(mu)
+
     # diffs = [loss - min_loss for i, loss in enumerate(losses) if i != min_i]
     # total_diff = sum(diffs)
     # normal_diff = [diff / total_diff for diff in diffs]
@@ -180,12 +247,12 @@ def square_cb(
         for i, loss in enumerate(losses)
         if i != min_i
     ]
-    # probs.insert(min_i, 1 - sum(probs))
+    probs.insert(min_i, 1 - sum(probs))
     # total = sum(probs)
     # probs = [p / total for p in probs]
 
     plt.scatter(
-        x=[i for i in range(len(loader.dataset) - 1)],
+        x=[i for i in range(len(loader.dataset))],
         y=probs
     )
     plt.xlabel("Field (Ordinal)")
@@ -195,5 +262,20 @@ def square_cb(
 
     logger.add_figure(f"fig/square-cb-weights-vs-field-ordinal", fig)
     logger.flush()
+
+    logger.add_text(
+        tag="SquareCB",
+        text_string=f"Field with min MSE: {min_i}",
+        global_step=log_step
+    )
+    log_step += 1
+    for i, p in enumerate(probs):
+        logger.add_text(
+            tag="SquareCB",
+            text_string=f"Field {i} SquareCB weight: {p}",
+            global_step=log_step
+        )
+        log_step += 1
+        logger.flush()
 
     logger.close()
